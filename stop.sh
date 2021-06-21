89c2c9aec1ef0381a2269b837dbabc6ca7a7cf3e
luxq <xueqian1991@gmail.com>
2021-06-21 03:49:52 +0000

    update script

--- stop.sh
+++ stop.sh
@@ -4,9 +4,9 @@ source env.sh
 Off=${1:-0}
 N=${2:-1}
 
-for i in $(seq 0 $N)
+for i in $(seq 1 $N)
 do
-	idx=$(($Off + $i))
+	idx=$(($Off + $i - 1))
 	containname="bee-$groups-node-$idx"
 	echo "docker stop $containname"
 	docker stop $containname 
